dc7922e1b9aa24fe584a42f29140449715440566
Tatiana <tatyumi29@gmail.com>
2025-04-30 18:04:50 +0200

    recup param id

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,24 @@
 //Mettre le code JavaScript lié à la page photographer.html
+function getUrlParam() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
+    return id;
+}
+async function getMedia() {
+    const id = getUrlParam();
+    const results = await fetch('../data/photographers.json');
+    const json = await results.json();
+    const medias = json.media.filter((media) => media.photographerId === parseInt(id));
+    const photographer = json.photographers.filter((photographer) => photographer.id === parseInt(id))[0];
+    return {medias, photographer};  
+}
+async function init() {
+    // Récupère les datas des photographes
+    const {medias, photographer} = await getMedia();
+    console.log(medias, photographer);
+}
+
+init();
+
+
